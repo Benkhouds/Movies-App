2089840bf0d17afd23bfd2e2fa16857453ba776b
hamza <hamza.benkhoud@etudiant-isi.utm.tn>
2021-10-26 20:49:31 +0100

    improvements

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,6 +1,7 @@
 
 import {Link} from 'react-router-dom'
 import {useRef} from 'react'
+import placeholderImage from '../images/placeholder.jpg'
 
 export default function MovieItem(props){
     const placeholderRef = useRef();
@@ -15,18 +16,29 @@ export default function MovieItem(props){
       ratingColor = "text-yellow-400"
     }
     function showImage(e){
+      if(e.target.src === window.location.origin+ placeholderImage){
+         e.target.className+= " filter blur-sm";
+      }
       e.target.classList.remove('hidden')
       placeholderRef.current.classList.add('hidden');
-
+      
+    }
+    function imageErrorHandler(e){
+       e.target.setAttribute('src', placeholderImage);
     }
     return(
         <div key={props.data.id} className="group overflow-hidden relative">
-          <img className="hidden" src={poster_path ? `${baseUrl}${poster_path}`: '../cine.jpg'} style={{aspectRatio:2/3}} alt={props.data.title} onLoad={showImage} />
-
+          <img className="hidden" src={poster_path ? `${baseUrl}${poster_path}`: placeholderImage} 
+              style={{aspectRatio:2/3}} 
+              alt={props.data.title} 
+              onLoad={showImage} 
+              onError={imageErrorHandler}
+          />
           <div className="bg-gray-800 filter blur relative" style={{aspectRatio:"2/3"}} ref={placeholderRef}>
             <div className="activity"></div>
           </div>
-          <div className=" absolute bg-gray-900 transform translate-y-full 
+          
+          <div className="absolute bg-gray-900 transform translate-y-full 
                     group-hover:translate-y-0 transition-transform text-white 
                     duration-300
                     w-full p-4 bg-opacity-70 bottom-0 z-20"
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,93 +1,143 @@
 
-import {  useEffect, useState } from 'react'
+import {  useEffect, useReducer , useRef} from 'react'
 import { useParams } from 'react-router-dom'
 import Spinner from '../components/UI/Spinner'
-import Error from '../components/UI/Error'
+import ErrorElement from '../components/UI/Error'
 import Layout from '../components/layout/Layout'
 
+
+
 const baseURL = "https://api.themoviedb.org/3";
+
+
+/* Reducer for Handling state */
+function reducer(state, action){
+    switch(action.type){
+        case 'movieData':{
+            return {...state ,
+                     isLoading: false,
+                     error:'', 
+                     synopsis :action.setSynopsis,
+                     title:action.setTitle,
+                     videoKey : action.setVideoKey, 
+                    }
+        }
+        case 'initialize':{
+            return {...state, isLoading:true , error:''}
+        }
+        case 'loadState':{
+            return {...state, isLoading : action.setIsLoading }
+        }
+        case 'error':{
+           return {...state, error: action.setError,isLoading: false}
+        }
+        default :{
+            return {...state, isLoading:false, error : "no action found"}
+        }
+
+    }
+}
+
+/* Initial states that will be passed to the reducer  */
+const initialState ={isLoading:true , error:'',synopsis:'', title:'', videoKey:'' }
+
+
+/* React component */
 export default function MovieDetails(){
-    const {id} = useParams()
-    const [isLoading, setIsLoading] = useState(true)
-    const [synopsis , setSynopsis] = useState('')
-    const [title, setTitle] = useState('') 
-    const [error , setError] = useState('')
-    const [videoKey, setVideoKey] = useState(null)
-     
+    const placeholderRef = useRef();
+    const {id} = useParams()  
+    const [state, dispatch] = useReducer(reducer,initialState)
+    //Destructuring the state
+    const {isLoading, error , title , synopsis, videoKey} = state;
+
     useEffect(()=>{
+        //initializing the loading state to false and error to empty string 
+        //every time the id in the url changes 
+        dispatch({type:'initialize'});
+
+        //each fetch return a promise that we capture into the promises array
         const promises = [
                     fetch(`${baseURL}/movie/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`),
                     fetch(`${baseURL}/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`),
                 ]
+        //handling of multiple promises at the same time        
         Promise.all(promises)    
-             .then((responses)=>{
-                return Promise.all(
-                     responses.map((res)=>{
-                         if(!res.ok){
-                            setError('error fetching data')
-                         }
-                         return res.json()
-                     })
-                 )     
-             })
+             .then(responses => Promise.all(responses.map(res => res.json())))
              .then((results)=>{
-                 console.log(results[1])   
-                 setIsLoading(false)
-                 setTitle(results[1].original_title)
-                if(!results[1]?.overview){
-                    setError("couldn't find overview")
+                //the results variable is an array of the data from both api endpoints
+                //so the data from the first endpoint would be stored in results[0] 
+                console.log(results); 
+                const [movieTrailerResults, movieInfo ] = results;
+                 if("success" in movieInfo && !movieInfo.success){
+                     throw new Error(movieInfo.status_message);
+                 }
+                 //the movie trailer object has a results key which is an array of multiple providers
+                 //of the movie trailer info  , we can simply choose the first one
+                 //if there's no result we simply set an empty string to state
+                 //so that even if we couldn't find a trailer for the movie we can still show the overview  
+                 const movieTrailer =movieTrailerResults.results?.length ? movieTrailerResults.results[0] : {key:''};
+                 
+                //we check if the dataset is not empty before updating the state  
+                //we set the loading state to false on both error and movieData action types
+                if(!movieInfo.overview || !movieInfo.original_title ){
+                     throw new Error("couldn't find related data");
                 }else{
-                    setSynopsis(results[1].overview)
+                    dispatch({
+                            type:'movieData',
+                            setTitle:  movieInfo.original_title,
+                            setSynopsis : movieInfo.overview,
+                            setVideoKey : movieTrailer.key
+                    })
                 }    
-                if(!results[0]?.results[0]?.key){
-                   setError('no trailer was found')
-                }else{
-                    setVideoKey(results[0].results[0].key)
-                }
-                
-                   
-                console.log(results)
+    
              })
-             .catch((err)=>{
-                 setError('error fetching data');
-                 setIsLoading(false);
+             .catch((err)=>{           
+                 dispatch({type:'error', setError:err.message ?? 'error fetching data'}); 
              })
 
     },[id])
+
     function showIFrame(e){
-       console.log(e)
+       e.target.classList.remove('hidden');
+       placeholderRef.current.classList.add('hidden');
+
     }
+
     return(
         <Layout>
-            {error && <Error message={error}/>}  
+            {error && <ErrorElement message={error}/>}  
             {isLoading && <Spinner/>}
-            <main className="px-16 py-4">
-                <div className="relative mb-12">
-                    <h1 className="text-white text-3xl font-semibold ">
-                        <span className="absolute block theme-color w-36 h-4 -bottom-1 -z-10"></span>
-                        {title}
-                    </h1>
-                </div>
-                {!error &&
-                    <div className="p-2 grid grid-cols-2 gap-8 items-center">
-                        <iframe 
-                                onLoad={showIFrame}  
-                                width="560" 
-                                height="315" 
-                                src={`https://www.youtube.com/embed/${videoKey}`} 
-                                title="YouTube video player" 
-                                frameBorder="0" 
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                allowFullScreen
-                        >
-                        </iframe>
-                        <p>
-                            {synopsis}
-                        </p>
+            {!error && !isLoading && 
+                <main className="px-16 py-4">
+                        <h1 className="title text-white text-3xl font-semibold mb-12">
+                            {title}
+                        </h1>
+                        <div className="p-2 grid grid-cols-2 gap-8 items-start w-full h-96">
+                            <div className="w-full h-full relative bg-gray-800 overflow-hidden">
+                                <div className="activity" ref={placeholderRef}></div>
+                                <iframe 
+                                        onLoad={showIFrame}  
+                                        className="hidden h-full w-full"
+                                        src={`https://www.youtube.com/embed/${videoKey}`} 
+                                        title="YouTube video player" 
+                                        frameBorder="0" 
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                        allowFullScreen
+                                >
+                                </iframe>
 
-                    </div>
-                }
-            </main>
+                            </div>
+                            <div className="pl-4 pr-8 lg:w-3/4">
+                                <h1 className="title font-semibold text-2xl mb-6 text-gray-200 text-opacity-90 ">Overview</h1>
+                                <p className="text-white leading-relaxed tracking-wide ">
+                                    {synopsis}
+                                </p>
+
+                            </div>
+
+                        </div>
+                </main>
+            }          
         </Layout>
     )
 }
